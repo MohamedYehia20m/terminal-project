ffc9a754dfaae51b3d4ff0e6bec35d7ab1941ff4
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-07 02:20:39 -0700

    modify _putchar

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,18 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	return (write(1, &c, 1));
 }
