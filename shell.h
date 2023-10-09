d9b294b4ba60753034d7ea948899feed3ddb8d42
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-09 11:25:31 -0700

    add functions headers

--- shell.h
+++ shell.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stddef.h>
 
 /*macros*/
 #define WRITE_BUF_SIZE 1024
@@ -21,13 +22,18 @@ struct Node {
 /*previous functions*/
 void _puts(char *s);
 int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 /*new functions*/
 char* _getline(void);
 char** split_string(char* str, const char *delim);
 int _putchar(char c);
-void check_command(char *command);
+/*void check_command(char *command);*/
 char *_getenv(char *name);
 void printPathDirectories(void);
+int check_spaces_input(char *buffer);
+char *check_command(char *command);
 
 #endif
