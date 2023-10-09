a790af686b6d149c08c543dec683398b78dd7f91
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-09 15:02:59 +0300

    add _strlen() function

---
+++ _strlen.c
@@ -0,0 +1,20 @@
+/**
+ * strlen - a function that returns the length of a string.
+ * @s: to char pointer
+ *  
+ * Description: a function that returns the length of a string
+ * by looping through its chracters till the end
+ *
+ * Return: return counter of type int
+ */
+
+ int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
