658132e2191b5b45360525dd4f2e2cdfd1027ea1
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-11 13:36:56 -0700

    add function to handle path

---
+++ handle_path.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * handle_path - handle the path function
+ * @argv: Arguments array
+ * @bf: pointer to the buffer
+ *
+ * Return: full path for the command
+ */
+char *handle_path(char **argv, char *bf)
+{
+	char *path;
+
+	path = malloc(_strlen("/bin/") + _strlen(bf) + 1);
+	if (!path)
+		return (NULL);
+	_strcpy(path, "/bin/");
+	if (bf[0] != '/' && bf[0] != '.')
+	{
+		argv[0] = _strcat(path, bf);
+		return (argv[0]);
+	}
+	free(path);
+	return (bf);
+}
