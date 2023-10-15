84b5f73e078d31892b62fa744ee8bcb8f2511943
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-15 04:18:50 -0700

    add function to check if input is builtin function

---
+++ check_builtin.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * check_builtin - checks if the command one of the builtin functions
+ * @argv: pointer to buffer
+ *
+ *  Return: void
+ */
+void check_builtin(char **argv)
+{
+	int i;
+	builtin_t builtinArr[] = {
+		{"exit", myExit},
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtinArr[i].cmd; i++)
+		if (_strcmp(argv[0], builtinArr[i].cmd) == 0)
+			builtinArr[i].func(argv);
+}
