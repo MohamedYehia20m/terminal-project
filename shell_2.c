45a0748324474893885ce19520214454b5ea3281
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-15 08:05:02 -0700

    remove comments

--- shell_2.c
+++ shell_2.c
@@ -16,37 +16,17 @@ int main(int ac, char **av)
 	while (1)
 	{
 		if (interactive && isatty(STDIN_FILENO))
-			_puts("$ "); // write(STDOUT_FILENO, "$ ", _strlen("$ "));
-		// signal(SIGINT, sig_handler);
-		buffer = _getline(); // getline(&cmd, &n, stdin);
-		/*
-		if (num_c == -1) // handles the end file case
-		{
-			free(cmd);
-			exit(EXIT_SUCCESS);
-		}
-		*/
+			_puts("$ ");
+		buffer = _getline();
 		buflen = _strlen(buffer);
 		if (buffer[buflen - 1] == '\n')
 			buffer[buflen - 1] = '\0';
 		buffer = remove_spaces(buffer); // trim(cmd);
 		if (_strlen(buffer) == 0)
 			continue;
-		/*
-		i = 0;
-		argv[i] = strtok(buffer, " \n");
-		// handle_exit(cmd);
-		handle_path(argv, cmd);
-		while (argv[i])
-		{
-			i++;
-			argv[i] = strtok(NULL, " \n");
-		}
-		*/
 		argv = split_string(buffer, " \t\n");
 		check_builtin(argv);
 		handle_path(argv, buffer);
-		// runcmd(argv, av, envp);
 		execute_command(argv);
 	}
 	free(buffer);
