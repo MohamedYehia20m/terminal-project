f0a6013685774bbd43035713f0b2773d55c0d8d2
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-19 00:34:52 +0300

    removing unused file

--- max_pid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the maximum process ID value from the kernel.
-max_pid=$(cat /proc/sys/kernel/pid_max)
-
-# Print the maximum process ID value to the console.
-echo "Maximum process ID value: $max_pid"
