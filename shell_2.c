95a77cff0bd56ad559f58855a1f8f782605e325d
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-19 01:53:44 +0300

    some modifications

--- shell_2.c
+++ shell_2.c
@@ -7,19 +7,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int ac, __attribute__((unused))char **av)
+int main(int ac, char **av)
 {
 	int interactive = (ac == 1 && isatty(STDIN_FILENO));
-	int buflen = 0;
-	char *buffer = NULL, __attribute__((unused))**argv = NULL;
+	size_t buflen = 0;
+	char *buffer = NULL, **argv = NULL;
 
-	/*
 	if (ac == 2)
 	{
 		execute_from_file(av[1]);
 		return (0);
 	}
-	*/
 	while (1)
 	{
 		if (interactive && isatty(STDIN_FILENO))
@@ -28,18 +26,16 @@ int main(int ac, __attribute__((unused))char **av)
 		buflen = _strlen(buffer);
 		if (buffer[buflen - 1] == '\n')
 			buffer[buflen - 1] = '\0';
-		/*remove_spaces(buffer);*/
-		/*buffer = delete_comments(buffer);*/
+		remove_spaces(buffer);
+		buffer = delete_comments(buffer);
 		if (_strlen(buffer) == 0)
 			continue;
-		/*argv = split_string(buffer, " \t\n");*/
-		/*if (check_builtin(argv))
-			continue;*/
-		/*handle_path(argv, buffer);*/
-		execute_command(/*argv*/&buffer);
+		argv = split_string(buffer, " \t\n");
+		if (check_builtin(argv))
+			continue;
+		handle_path(argv, buffer);
+		execute_command(argv);
 	}
 	free(buffer);
-	/*while (argv)
-		free(*(argv++));*/
 	return (0);
 }
