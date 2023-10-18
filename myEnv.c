a69d3a1a94d5b424028c2e2763d1336b88547b34
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-19 01:02:50 +0300

    fixing betty documentation and code style for myEnv() function

--- myEnv.c
+++ myEnv.c
@@ -1,14 +1,20 @@
 #include "shell.h"
 
+/**
+ * myEnv - function that returns all environment variables in the Environment
+ * @argv: pointer to pointer (unused attribute)
+ *
+ * Return: void
+ */
+
 void myEnv(__attribute__((unused)) char **argv)
 {
-	extern char **environ;
- 	int i;
+	int i;
 
 	i = 0;
 	while (environ[i])
 	{
-		 _puts(environ[i++]);
-		 _putchar('\n');
+		_puts(environ[i++]);
+		_putchar('\n');
 	}
 }
