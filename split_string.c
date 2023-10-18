757e220882a6fff571457a7100b85b2f2682b5d6
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-19 01:09:10 +0300

    fixing betty documentation and code style

--- split_string.c
+++ split_string.c
@@ -3,6 +3,7 @@
 /**
  * split_string - function that split string into array of words
  * @str: string to be split
+ * @delim: the delimiter based on which the string is tokenized
  *
  * Description: the functions splits string into array of words using
  * strtok() function based on standard delimeter " ".
@@ -10,21 +11,21 @@
  * Return: array of words.
  */
 
-char **split_string(char *str , const char *delim)
+char **split_string(char *str, const char *delim)
 {
-	static char* words[MAX_WORDS];
-        int num_words = 0;
-        /*char **words = malloc(sizeof(char *) * 1024);*/
-        char *token = strtok(str, delim);
+	static char *words[MAX_WORDS];
+	int num_words = 0;
+	/*char **words = malloc(sizeof(char *) * 1024);*/
+	char *token = strtok(str, delim);
 
-        while (token && (num_words < MAX_WORDS - 1))
-        {
-                words[num_words] = token;
+	while (token && (num_words < MAX_WORDS - 1))
+	{
+		words[num_words] = token;
 		token = NULL;
-                token = strtok(NULL, delim);
-                num_words++;
-        }
-        words[num_words] = NULL;
+		token = strtok(NULL, delim);
+		num_words++;
+	}
+	words[num_words] = NULL;
 	token = NULL;
-        return (words);
+	return (words);
 }
