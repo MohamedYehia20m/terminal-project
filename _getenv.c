cd119073871e9ac7dd4b6462b2cc27000d45ec59
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-19 00:46:33 +0300

    fixing betty documentation and code style

--- _getenv.c
+++ _getenv.c
@@ -1,16 +1,22 @@
 #include "shell.h"
 
+/**
+ * _getenv - function that return the value of given environment variable
+ * @name: name of environment variable
+ *
+ * Return: value of environment variable
+ */
+
 char *_getenv(char *name)
 {
-	extern char **environ;
 	int i;
 	char *token;
 
 	i = 0;
 	while (environ[i])
 	{
-		token = strtok(environ[i++], "="); 
-		if (_strcmp(token, name) == 0) 
+		token = strtok(environ[i++], "=");
+		if (_strcmp(token, name) == 0)
 			return (strtok(NULL, "="));
 	}
 	return (NULL);
