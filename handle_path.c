523dfddb3e6237aac25430e1508493caa16c47e1
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-19 00:58:35 +0300

    fixing betty documentation and code stylefor handle_path() function

--- handle_path.c
+++ handle_path.c
@@ -8,49 +8,17 @@
  * Return: full path for the command
  */
 
-/*char *handle_path(char **argv, char *bf)
-{
-	char *path = NULL;
-
-	path = malloc(_strlen("/bin/") + _strlen(bf) + 1);
-	if (!path)
-		return (NULL);
-	_strcpy(path, "/bin/");
-	if (bf[0] != '/' && bf[0] != '.')
-	{
-		argv[0] = _strcat(path, bf);
-		path = NULL;
-		return (argv[0]);
-	}
-	free(path);
-	return (bf);
-}*/
-
 char *handle_path(char **argv, char *bf)
 {
 	static char path[100];  /* Assuming a maximum path length of 100 characters */
 	char *result = NULL;
 
 	if (bf[0] != '/' && bf[0] != '.')
-	{	
-		_strcpy(path, "/bin/");	
-		result = _strcat(path, bf);				
-		argv[0] = result;					
+	{
+		_strcpy(path, "/bin/");
+		result = _strcat(path, bf);
+		argv[0] = result;
 		return (argv[0]);
 	}
-	return(bf);
+	return (bf);
 }
-
-/*
-int main()
-{
-    char* argv[1];
-        char bf[] = "program";
-	    
-	    char* result = handle_path(argv, bf);
-	        
-	        printf("Result: %s\n", result);
-		    
-		    return 0;
-}
-*/
